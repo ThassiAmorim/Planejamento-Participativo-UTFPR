ffc84f7cd7feda43060f3c815ffb6839748f9c50
Thassiana Camilia Amorim Muller <thassi.ana.amorim@gmail.com>
2024-04-16 11:13:17 -0300

    Alter possible activity_types in activity form

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,9 @@ class HomeController < ApplicationController
   def index
     @activities = Activity.all
     @activities_total =  @activities.count
-    @activities_mestrados = @activities.where(activity_type: "Mestrado").count
-    @activities_ic = @activities.where(activity_type: "Pós-Graduação").count
-    @activities_pos = @activities.where(activity_type: "Iniciação científica").count
+    @activities_infra = @activities.where(activity_type: "Infraestrutura").count
+    @activities_graduacao = @activities.where(activity_type: "Graduação").count
+    @activities_pos = @activities.where(activity_type: "Pós-Graduação").count
     @activities_ext = @activities.where(activity_type: "Extensão").count
   end
 end
